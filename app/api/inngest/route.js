0aa83fbd4361bc4beb4e3e355f9ba7bfce5bfd15
uday <dunna.udaykiran1729@gmail.com>
2026-01-06 14:33:41 +0530

    inn2

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,5 +1,6 @@
 import { serve } from "inngest/next";
-import { inngest } from "../../../inngest/client";
+// import { inngest } from "../../../inngest/client";
+import { inngest } from "@/inngest/client";
 import { helloWorld } from "/inngest/functions";
 import { CreateNewUser } from "/inngest/functions";
 import { GenerateNotes } from "/inngest/functions";
