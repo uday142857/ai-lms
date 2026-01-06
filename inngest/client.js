4b9b363d74dbac3f4cb4f2754bc2f81a60dd40c9
uday <dunna.udaykiran1729@gmail.com>
2026-01-06 14:32:02 +0530

    inngest/client

--- inngest/client.js
+++ inngest/client.js
@@ -1,4 +1,10 @@
-import { Inngest } from "inngest";
+// import { Inngest } from "inngest";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "ai-lms" });
+// export const inngest = new Inngest({ id: "ai-lms" });
+
+import { Inngest } from "inngest";
+
+export const inngest = new Inngest({
+  id: "ai-lms",
+});
