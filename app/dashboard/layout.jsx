f61d1c83c9c5b7b2849353d298618f9fa5dbc6cb
uday <dunna.udaykiran1729@gmail.com>
2026-01-07 12:49:59 +0530

    inn5

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import SideBar from './_components/SideBar'
-import DashboardHeader from './_components/DashboardHeader'
+"use client";
+import React from "react";
+import SideBar from "./_components/SideBar";
+import DashboardHeader from "./_components/DashboardHeader";
 
-function DashboardLayout({children}) {
+function DashboardLayout({ children }) {
   return (
-    <div className='h-screen'>
-      <div className='md:w-64 hidden md:block fixed'>
+    <div className="h-screen">
+      <div className="md:w-64 hidden md:block fixed">
         <SideBar />
       </div>
-      <div className='md:ml-64'>
+      <div className="md:ml-64">
         <DashboardHeader />
-        <div className='p-10'>
-            {children}
-        </div>
+        <div className="p-10">{children}</div>
       </div>
     </div>
   );
 }
 
-export default DashboardLayout
+export default DashboardLayout;
